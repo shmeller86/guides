e9af88ffe0918fd001b6820047d6622eac2ea512
Ïgor Rogachevich <shmeller86@gmail.com>
2022-05-31 23:11:37 +0800

    fix url

--- subspace.sh
+++ subspace.sh
@@ -1,6 +1,6 @@
 cd ~
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-node-ubuntu-x86_64-gemini-1a-2022-may-31
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-farmer-windows-x86_64-gemini-1a-2022-may-31.exe
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-farmer-ubuntu-x86_64-gemini-1a-2022-may-31
 mv subspace* /usr/local/bin/
 chmod +x /usr/local/bin/subspace*
 adduser --system --home=/var/lib/subspace subspace
