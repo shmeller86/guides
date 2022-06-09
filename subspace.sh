b22dd2e609d2a0d9a7b87263fd01233c102de88f
Ïgor Rogachevich <shmeller86@gmail.com>
2022-06-09 23:47:28 +0800

    update

--- subspace.sh
+++ subspace.sh
@@ -56,6 +56,7 @@ echo "export SUBSPACE_NODE_NAME="$SUBSPACE_NODE_NAME"" >> ~/.bash_profile
 
 cd ~
 VERSION=$(wget -qO- https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name")
+VERSION=gemini-1b-2022-jun-08
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/$VERSION/subspace-node-ubuntu-x86_64-$VERSION
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/$VERSION/subspace-farmer-ubuntu-x86_64-$VERSION
 mv subspace* /usr/local/bin/
